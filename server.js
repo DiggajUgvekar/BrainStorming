38104d68932541f90f8fd0505f4c769636e316f0
Saish Shirodkar <shirodkarsaish00@gmail.com>
2024-07-12 21:50:11 +0530

    created templates

--- server.js
+++ server.js
@@ -13,17 +13,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', questionRoutes);
 
 app.get('/', (req, res) => {
-  res.sendFile('public/index.html', {root:__dirname})
+  res.sendFile('public/templates/index.html', {root:__dirname})
 })
 
 
 app.get('/level', (req, res) => {
-  res.sendFile('public/level.html', {root:__dirname})
+  res.sendFile('public/templates/level.html', {root:__dirname})
 })
 
 
 app.get('/quiz', (req, res) => {
-  res.sendFile('public/quiz.html', {root:__dirname})
+  res.sendFile('public/templates/quiz.html', {root:__dirname})
 })
 
 app.listen(port, () => {
